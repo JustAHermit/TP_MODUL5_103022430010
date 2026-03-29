bb6d775b85c9d841731bab71934823b15e8f47b0
JustAHermit <arsall1202@gmail.com>
2026-03-29 23:00:34 +0700

    menambahkan class DataGeneric

--- TP_MODUL5_103022430010/Program.cs
+++ TP_MODUL5_103022430010/Program.cs
@@ -1,1 +1,24 @@
-﻿Console.WriteLine("Hello, World!");
+﻿public class DataGeneric<T>
+{
+    private T data;
+    public DataGeneric(T inputData)
+    {
+        data = inputData;
+    }
+
+    public void PrintData()
+    {
+        Console.WriteLine("Data yang tersimpan adalah: " + data);
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        string perusahaan = "Limbus Company";
+
+        DataGeneric<string> dataperusahaan = new DataGeneric<string>(perusahaan);
+        dataperusahaan.PrintData();
+    }
+}
--- TP_MODUL5_103022430010/obj/Debug/net10.0/TP_MODUL5_103022430010.AssemblyInfo.cs
+++ TP_MODUL5_103022430010/obj/Debug/net10.0/TP_MODUL5_103022430010.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TP_MODUL5_103022430010")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4d6dc3e2c80928848280b9fda886696a9724eb34")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7c36697570b636bfb8bc78699ff3778e6bc974c1")]
 [assembly: System.Reflection.AssemblyProductAttribute("TP_MODUL5_103022430010")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TP_MODUL5_103022430010")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
